9748fc192490c174b58b6ebb0432198c9da3dc59
samphony1 <samirelborno@gmail.com>
2026-07-02 00:57:09 +0200

    feat: add Eltern Infos pattern, clean up page 209 hero, and set equal height columns

---
+++ patterns/section-eltern-infos.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: Eltern Infos (GEV & Links)
+ * Slug: eliashof/section-eltern-infos
+ * Categories: eliashof-sections
+ * Description: Two-column section for parent GEV contact info and important external links. White background with grid pattern.
+ */
+?>
+<!-- wp:group {"align":"full","anchor":"section-eltern-infos","className":"eliashof-section section-eltern-infos bg-graph-white","layout":{"type":"constrained"}} -->
+<div id="section-eltern-infos" class="wp-block-group alignfull eliashof-section section-eltern-infos bg-graph-white">
+	<!-- wp:group {"className":"eliashof-eltern-infos-container","layout":{"type":"default"}} -->
+	<div class="wp-block-group eliashof-eltern-infos-container">
+		
+		<!-- wp:heading {"level":2,"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"52px"}}}} -->
+		<h2 class="wp-block-heading has-text-align-center" style="margin-top:0;margin-bottom:52px">ELTERN INFOS</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:columns {"verticalAlignment":null,"className":"eliashof-eltern-infos-columns","style":{"spacing":{"blockGap":"60px"}}} -->
+		<div class="wp-block-columns are-vertically-aligned-stretch eliashof-eltern-infos-columns">
+
+			<!-- Left Column: GEV Contact Info -->
+			<!-- wp:column {"verticalAlignment":null,"className":"eliashof-eltern-infos-col"} -->
+			<div class="wp-block-column eliashof-eltern-infos-col">
+				<!-- wp:heading {"level":3,"textAlign":"center","className":"eliashof-eltern-infos-subtitle"} -->
+				<h3 class="wp-block-heading has-text-align-center eliashof-eltern-infos-subtitle">ANSPRECHPARTNERINNEN</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"className":"eliashof-eltern-infos-item"} -->
+				<p class="eliashof-eltern-infos-item">GEV-VORSITZ: Frau Fosco<br>STELLVERTRETUNG: Frau Zitzmann, Frau Kern<br>KONTAKT: <a href="mailto:gev-vorstand@eliashof.net">gev-vorstand@eliashof.net</a></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+
+			<!-- Right Column: Links -->
+			<!-- wp:column {"verticalAlignment":null,"className":"eliashof-eltern-infos-col"} -->
+			<div class="wp-block-column eliashof-eltern-infos-col">
+				<!-- wp:heading {"level":3,"textAlign":"center","className":"eliashof-eltern-infos-subtitle"} -->
+				<h3 class="wp-block-heading has-text-align-center eliashof-eltern-infos-subtitle">LINKS</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"className":"eliashof-eltern-infos-item"} -->
+				<p class="eliashof-eltern-infos-item"><a href="#">MITWIRKUNG VON ELTERN IN DER SCHULE</a><br><br><a href="#">AKTUELLES VOM BEZIRKSELTERNAUSSCHUSS:</a><br><br><a href="#">AKTUELLES VOM LANDESELTERNAUSSCHUSS</a><br><br><a href="#">AKTUELLE ANGEBOTE DES SIBUZ FÜR ELTERN</a></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:column -->
+
+		</div>
+		<!-- /wp:columns -->
+
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
